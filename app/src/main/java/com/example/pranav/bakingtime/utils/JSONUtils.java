1fef67b877692fd857f896dcfa72be9f8ef1959a
pranavd <pranavdixit08@gmail.com>
2018-04-27 04:26:20 -0700

    add string constants, add a function to get the recipe name, remove unwanted functions

--- app/src/main/java/com/example/pranav/bakingtime/utils/JSONUtils.java
+++ app/src/main/java/com/example/pranav/bakingtime/utils/JSONUtils.java
@@ -11,18 +11,33 @@ public class JSONUtils {
 
     private static final String TAG = JSONUtils.class.getSimpleName();
 
+    public static final String NAME_KEY = "name";
+    private static final String STEPS_KEY = "steps";
+    private static final String INGREDIENTS_KEY  = "ingredients";
+    private static final String QUANTITY_KEY = "quantity";
+    private static final String  MEASURE_KEY = "measure";
+    private static final String INGREDIENT_KEY = "ingredient";
+    private static final String SHORT_DESCRIPTION_KEY = "shortDescription";
+
 
     public static String[] getRecipesFromJSON(String recipeJsonResponse) throws JSONException {
 
         JSONArray recipeArray = new JSONArray(recipeJsonResponse);
         String[] recipes = new String[recipeArray.length()];
-
         for(int i =0 ; i< recipeArray.length();i++){
             JSONObject recipeObject = recipeArray.getJSONObject(i);
-            recipes[i] = recipeObject.getString("name");
+            recipes[i] = recipeObject.getString(NAME_KEY);
         }
-        Log.d(TAG, "getRecipesFromJSON: " + recipes[0] + " to " + recipes[recipeArray.length()-1]);
         return recipes;
+
+    }
+
+    public static String getRecipesNameJSON(JSONObject obj) throws JSONException {
+        if(obj==null){
+            return null;
+        }
+        String name = obj.getString(NAME_KEY);
+        return name;
 
     }
 
@@ -31,18 +46,17 @@ public class JSONUtils {
         if(position == 0 ){
         return null;
         }
-        String value = obj.getJSONArray("steps").getJSONObject(position-1).getString(key);
-
+        String value = obj.getJSONArray(STEPS_KEY).getJSONObject(position-1).getString(key);
         return value;
 
     }
 
     public static int numStepsRecipe(JSONObject recipe) throws JSONException {
-
-        JSONArray stepArray = recipe.getJSONArray("steps");
-
+        JSONArray stepArray = recipe.getJSONArray(STEPS_KEY);
         return  stepArray.length();
     }
+
+
     public static JSONArray getRecipeDetails(String recipeJsonResponse) throws JSONException {
 
         JSONArray recipeArray = new JSONArray(recipeJsonResponse);
@@ -51,104 +65,47 @@ public class JSONUtils {
     }
 
     public static String getIngredientsFromJSON(JSONObject recipe) throws JSONException {
-
-        JSONArray ingredientArray= recipe.getJSONArray("ingredients");
-
+        if(recipe==null){
+            return null;
+        }
+        JSONArray ingredientArray= recipe.getJSONArray(INGREDIENTS_KEY);
         String ingredients = "";
 
         for(int i =0; i< ingredientArray.length();i++){
             JSONObject ingredientObj = ingredientArray.getJSONObject(i);
-            int quantity = ingredientObj.getInt("quantity");
-            String measure = ingredientObj.getString("measure");
-            String ingredient = ingredientObj.getString("ingredient");
-
+            int quantity = ingredientObj.getInt(QUANTITY_KEY);
+            String measure = ingredientObj.getString(MEASURE_KEY);
+            String ingredient = ingredientObj.getString(INGREDIENT_KEY);
             ingredients+= "  "+quantity + " " + measure + " " + ingredient + "\n\n";
-
         }
-
         return ingredients;
     }
 
     public static String getIngredientNamesFromJSON(JSONObject recipe) throws JSONException {
-
-        JSONArray ingredientArray= recipe.getJSONArray("ingredients");
-
+        JSONArray ingredientArray= recipe.getJSONArray(INGREDIENTS_KEY);
         String ingredients = "";
-
         for(int i =0; i< ingredientArray.length();i++){
             JSONObject ingredientObj = ingredientArray.getJSONObject(i);
-            String ingredient = ingredientObj.getString("ingredient");
-
+            String ingredient = ingredientObj.getString(INGREDIENT_KEY);
             ingredients+= (i+1) + ". " + ingredient + "\n";
-
         }
-
         return ingredients;
     }
 
     public static String[] getStepsFromJSON(JSONObject recipe) throws JSONException {
-
-        JSONArray stepArray= recipe.getJSONArray("steps");
-
+        if(recipe==null){
+            return null;
+        }
+        JSONArray stepArray= recipe.getJSONArray(STEPS_KEY);
         String[] steps = new String[stepArray.length()];
-
         for(int i =0; i< stepArray.length();i++){
             JSONObject stepObj = stepArray.getJSONObject(i);
-            String step = stepObj.getString("shortDescription");
-
+            String step = stepObj.getString(SHORT_DESCRIPTION_KEY);
             steps[i] = step;
-
         }
         return steps;
     }
 
-    public static String[] getIngrediantsAndStepsFromJSON(JSONObject recipe) throws JSONException {
-
-        JSONArray stepArray= recipe.getJSONArray("steps");
-
-        String[] ingredientAndSteps = new String[stepArray.length() + 1 ];
-
-        ingredientAndSteps[0] = getIngredientsFromJSON(recipe);
-        int i=1;
-
-        for( String str : getStepsFromJSON(recipe)){
-            ingredientAndSteps[i] = str;
-            i++;
-        }
-
-        return ingredientAndSteps;
-
-    }
-
-
-//
-//    public static String[] getRecipeIngredientsFromJSON(String recipeJsonResponse) throws JSONException {
-//
-//        JSONArray recipeArray = new JSONArray(recipeJsonResponse);
-//        String[] recipes = new String[recipeArray.length()];
-//
-//        for(int i =0 ; i< recipeArray.length();i++){
-//            JSONObject recipeObject = recipeArray.getJSONObject(i);
-//            recipes[i] = recipeObject.getString("name");
-//        }
-//        Log.d(TAG, "getRecipesFromJSON: " + recipes[0] + " to " + recipes[recipeArray.length()-1]);
-//        return recipes;
-//
-//    }
-//
-//    public static String[] getRecipesFromJSON(String recipeJsonResponse) throws JSONException {
-//
-//        JSONArray recipeArray = new JSONArray(recipeJsonResponse);
-//        String[] recipes = new String[recipeArray.length()];
-//
-//        for(int i =0 ; i< recipeArray.length();i++){
-//            JSONObject recipeObject = recipeArray.getJSONObject(i);
-//            recipes[i] = recipeObject.getString("name");
-//        }
-//        Log.d(TAG, "getRecipesFromJSON: " + recipes[0] + " to " + recipes[recipeArray.length()-1]);
-//        return recipes;
-//
-//    }
 
 
 
